5aa81b140750032e9003287d853ce9c976233a93
Kong Thiraphat <thiraphat.cto@gmail.com>
2024-10-03 12:45:16 +0700

    feat: add answer to exercise

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,21 @@
 // Start coding here
+let add = {
+    add (a,b) {
+        return a + b 
+    }
+}
+let subtract = {
+    subtract (a,b) {
+        return a - b
+    }
+}
+let multiply = {
+    multiply (a,b) {
+        return a * b
+    }
+}
+let divide = {
+    divide (a,b) {
+        return a / b
+    }
+}
